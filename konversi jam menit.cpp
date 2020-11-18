908b269cdf6b285b7864de3119edae2359e8b4a7
wina siti aditianingsih <rohmantaofik417@gmail.com>
2020-11-18 14:26:35 +0700

    add file konversi jam

---
+++ konversi jam menit.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+int main ()
+
+{
+	int j,m,d,td;
+	cout<<"masukan jam :" ;
+	cin>>j ;
+	cout<<"masukan menit :" ;
+	cin>>m;
+	cout<<"masukan detik :" ;
+	cin>>d;
+	td=(j * 3600) + (m * 60 )+ d;
+	cout<<"total detik: "<<td;
+	     return 0;
+}
