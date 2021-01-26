24cae7d0f5c4dd742f21c73437701eeff1228435
usupsuparma <usup.suparma7@gmail.com>
2021-01-26 20:09:46 +0700

    tugas 10

---
+++ a.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespase std;
+int main ;
+{
+	int i,n,t ;
+	cont << "masukan jumlah baris :" ;
+	cin >> "n" ;
+	for (i=1; i <= n; 1++) ;
+	{
+		for (i=1; i,= n; 1++ ) ;
+		{ 
+		cont << " * " ;
+		}
+		// bagian akhir 
+		cont "/n" ;
+	}
+	retunt 0
+	
+
+	
+}
+
+
+ 
